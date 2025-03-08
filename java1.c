dc28a9ebe9597e967c00b955352c7e1904be7488
anwesharoy09 <roysanta68@gmail.com>
2025-03-08 14:35:22 +0530

    fixes#1

--- java1.c
+++ java1.c
@@ -2,8 +2,8 @@ class public
 {
 	public static void main(int args[])
 	{
-		int a=2,b=2,sum;
-		sum=a+b;
+		int a=2,b=2,sum;#variable initialisation
+		sum=a+b;#adding two digits
 		println("sum="+sum);
 	}
 }
